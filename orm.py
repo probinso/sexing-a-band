99a891c9c4cf159dbe92631426bf0897310ecb06
probinso <pmoss.robinson@gmail.com>
2016-08-16 19:51:37 -0700

    orm makes file

--- orm.py
+++ orm.py
@@ -5,22 +5,52 @@ meta_db = Database("sqlite", 'data/track_metadata.db', create_db=False)
 class Song(meta_db.Entity):
     _table_ = "songs"
     track_id = PrimaryKey(str)
-    title = Required(str)
-    song_id = Required(str)
-    release = Required(str)
-    artist_id = Required(str)
-    artist_mbid = Required(str)
+    title = Optional(str)
+    #song_id = Required(str)
+    #release = Required(str)
+    #artist_id = Required(str)
+    #artist_mbid = Required(str)
     artist_name = Required(str)
-    duration = Required(float)
-    artist_familiarity = Required(float)
+    #duration = Required(float)
+    #artist_familiarity = Required(float)
     artist_hotttnesss = Required(float)
     year = Required(int)
-    track_7digitalid = Required(int)
-    shs_perf = Required(int)
-    shs_work = Required(int)
+    #track_7digitalid = Required(int)
+    #shs_perf = Required(int)
+    #shs_work = Required(int)
 
 sql_debug(False)
 meta_db.generate_mapping()
 
+word_db = Database("sqlite", 'data/mxm_dataset.db', create_db=False)
+
+class Word(word_db.Entity):
+    _table_ = 'words'
+    word = PrimaryKey(str)
+    lyrics = Set('Lyrics')
+
+class Lyrics(word_db.Entity):
+    _table_ = 'lyrics'
+    track_id = PrimaryKey(str)
+    mxm_tid = Required(int)
+    word = Required(str)
+    count = Required(int)
+    is_test = Required(int)
+    word = Required(Word)
+
+sql_debug(False)
+word_db.generate_mapping()
 
 
+@db_session
+def main():
+    words = select(e for e in Word)
+    songs = [(s.track_id, s.year) for s in select(e for e in Song if e.year)]
+    for track_id, year in songs:
+        words = [(w.word, c) for w, c in select((e.word, e.count) for e in Lyrics if e.track_id == track_id)]
+        d     = ','.join(['{}:{}'.format(*w) for w in words])
+        if d:
+            print(','.join([track_id, str(year), d]))
+
+
+main()

49394a777c19aedf22958a9f44ebeb170f8665cd
probinso <pmoss.robinson@gmail.com>
2016-08-18 20:57:45 -0700

    pants pants pants

---
+++ topic_year.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import gensim
+import csv
+
+print(gensim.__file__)
+
+lsi = gensim.models.LsiModel.load('./song_topics.lsi')
+
+fd = open('./data/mxm_dataset_train.txt')
+line = '#'
+it = iter(fd)
+while line[0] == '#':
+    line = next(it)
+
+DUMP_SIZE = 1000
+
+raw = list(enumerate(line[1:].strip().split(','),1))
+dump, keep = raw[:DUMP_SIZE], raw[DUMP_SIZE:]
+
+lookup = {k - DUMP_SIZE : v for k, v in keep}
+invert = {value:key for key, value in lookup.items()}
+
+"""
+create topic model
+test document wrt topic and histogram on year
+"""
+from collections import defaultdict
+d = defaultdict(list)
+with open("./data/output.csv"):
+    for line in csv.reader(fd):
+        year = line[1]
+        _ = map(lambda s: map(int, str.split(s, ':')), line[2:])
+        document = filter(lambda x : x[0] < DUMP_SIZE + 1, _)
+
+        topic = lsi[document]
+        d[year].append(topic)
+
+for i in sorted(d):
+    print(i)
+    print("  ", sorted(d[i]))
+    print()

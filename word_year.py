4ce013900c0f3bbd78e83718d6abe9004f21f918
Zachary Kent <zak.kent@gmail.com>
2016-10-11 11:45:49 -0700

    small change to count how many words were seen for the first time per decade

--- word_year.py
+++ word_year.py
@@ -18,7 +18,20 @@ def word_decade_dict_builder():
                 else:
                     output_dict[int(item[0])] = decade
 
-        print(output_dict)
+       
+       # -----------------------------------------------------
+        # year_breakdown = defaultdict(int)
+
+        # for item in output_dict.items():
+        #     year_breakdown[item[1]] += 1
+
+        # print(year_breakdown)
+        # -----------------------------------------------------
+        # The results below show the counts of when words were first seen in our corpus by decade
+        # {decade: count_of_words}
+        # result ----> defaultdict(<type 'int'>, {2: 804, 3: 374, 4: 445, 
+        #                           5: 1724, 6: 1192, 7: 447, 8: 14})
+
         return output_dict
 
 

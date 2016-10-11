58ca942288fe441b3b50fc81504853878c095b59
Zachary Kent <zak.kent@gmail.com>
2016-10-11 11:31:21 -0700

    changed file names and deleted some unused files

--- learning.py
+++
@@ -1,9 +0,0 @@
-import gensim
-
-def gensim_lsi_run(corpus, num_topics, id2word):
-    lsi = gensim.models.lsimodel.LsiModel(corpus, num_topics=num_topics, id2word=id2word
-    for index, topic in lsi.print_topics(num_topics=10, num_words = 3):
-        print(index)
-        print("  " + topic)
-
-    lsi.save('./song_topics.lsi')

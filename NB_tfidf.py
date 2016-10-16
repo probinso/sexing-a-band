c0d03617741f17aa4c29d168593e90d751acba4c
Zachary Kent <zak.kent@gmail.com>
2016-10-16 13:03:27 -0700

    made changes to train_data_tfidf to only grab tfidf scores for songs. NB_tfidf trains MultinomialNB using only tfidf scores in chunks

---
+++ NB_tfidf.py
@@ -0,0 +1,93 @@
+import csv
+import numpy as np 
+
+from scipy.sparse import coo_matrix
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+
+def get_data():
+    """grab data from csv and break into [X, y] list items"""
+    song_data = []
+    with open("./data/only_tfidf.csv") as fd:
+        for line in csv.reader(fd):    
+            # document = map(lambda s: map(float, str.split(s, ':')), line[3:])
+            document = [item.split(':') for item in line[3:]]        
+            X = dict([[int(item[0]), float(item[1])] for item in document])
+            y = int(line[2]) - 2
+
+            print(X)
+            break
+            
+            song_data.append([X, y])
+
+    return song_data
+ 
+def chunker(data, size):
+    """chunk data into 50 element lists for batch training"""
+    return ([data[pos:pos + size] for pos in xrange(0, len(data), size)])
+
+# change to each column having a word value, transpose what you have 
+def sparse_matrix(a_list):
+    """take list of [idx, tfidf scores] and convert to a sparse matrix with zeros included"""
+    row = np.array([item[0] for item in a_list])
+    col = np.array([0 for _ in range(len(row))])
+    data = np.array([item[1] for item in a_list])
+
+    try:
+        matrix = coo_matrix((data, (row, col)), shape=(5001,1))
+    except:
+        print('fail')
+
+    return matrix
+
+def run_NB():
+    """runs NB on chuncked data using partial_fit"""
+    clf = MultinomialNB(fit_prior=True)
+
+    classes = range(11)
+
+    for idx in range(len(train_data_50)):
+        print("start loop")
+        data = train_data_50[idx]
+
+        X_data = [item[0] for item in data]
+        y_data = np.array([item[1] for item in data])
+        print(y_data.shape)
+        
+        # had to reshape 3d array to 2d 
+        X_sparse = np.array(map(sparse_matrix, X_data))
+        new = X_sparse.reshape(50, 5001)
+
+        print(new.shape)
+
+        clf.partial_fit(new, y_data, classes=classes)
+
+
+def score():
+    # need to break out test data same as above 
+    X_test = [item[0] for item in test_data]
+    y_test = [item[1] for item in test_data]
+
+    X_test_sparse = np.array(map(sparse_matrix, X_test))
+    new = X_test_sparse.reshape(len(X_test_sparse), 5001)
+
+    score = clf.score(new, y_test)
+
+    print("NB score: {}".format(score))
+
+score()
+
+song_data = get_data()
+
+# spliting train/test 
+train_data = song_data[:500]
+test_data = song_data[500:550]
+ 
+train_data_50 = chunker(train_data, 50)
+
+
+
+
+
+
--- train_data_tfidf.py
+++ train_data_tfidf.py
@@ -4,12 +4,12 @@ import csv
 import gensim
 
 tfidf = gensim.models.TfidfModel.load("./full_tfidf_model.tfidf")
-lsi = gensim.models.LsiModel.load('./data/full_song_topics_tfidif.lsi')
+# lsi = gensim.models.LsiModel.load('./data/full_song_topics_tfidif.lsi')
 
 """Uses models trained in topic_maker_tfidf.py to set up data from our training data set 
     from ./data/mxm_dataset_train.txt so it can be used in another sklearn model more easily"""
 
-filename = 'data/full_output_tfidf.csv'
+filename = 'data/only_tfidf.csv'
 output_songs = []
 zeros = 0
 
@@ -19,17 +19,16 @@ with open(filename, 'w') as out_file:
         for line in csv.reader(fd):
 
             # used to filter out songs without date data 
-
             if line[2] == '0':
                 zeros += 1 
                 continue 
 
             song = map(lambda s: map(int, str.split(s, ':')), line[3:])
             song_tfidf = tfidf[song]
-            song_lsi_vec = lsi[song_tfidf]
+            # song_lsi_vec = lsi[song_tfidf]
             date_class = (int(line[2]) - 1900) // 10
 
-            topic_vec = ['{}:{}'.format(item[0], (item[1])) for item in song_lsi_vec]
+            topic_vec = ['{}:{}'.format(item[0], (item[1])) for item in song_tfidf]
 
             print(','.join([line[0], line[2], str(date_class), ','.join(topic_vec)]), file=out_file)
  

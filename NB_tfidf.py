4a9f6b900e8c884af0d76a0e5982e5e3acc6017f
Zachary Kent <zak.kent@gmail.com>
2016-10-16 13:37:13 -0700

    small changes to model to wrap use inside functions

--- NB_tfidf.py
+++ NB_tfidf.py
@@ -13,11 +13,8 @@ def get_data():
         for line in csv.reader(fd):    
             # document = map(lambda s: map(float, str.split(s, ':')), line[3:])
             document = [item.split(':') for item in line[3:]]        
-            X = dict([[int(item[0]), float(item[1])] for item in document])
+            X = [[int(item[0]), float(item[1])] for item in document]
             y = int(line[2]) - 2
-
-            print(X)
-            break
             
             song_data.append([X, y])
 
@@ -35,28 +32,29 @@ def sparse_matrix(a_list):
     data = np.array([item[1] for item in a_list])
 
     try:
-        matrix = coo_matrix((data, (row, col)), shape=(5001,1))
-    except:
-        print('fail')
+        matrix = coo_matrix((data, (row, col)), shape=(5001,1)).toarray()
+    except Exception:
+        print(Exception)
 
     return matrix
 
-def run_NB():
+def run_NB(train_data_50):
     """runs NB on chuncked data using partial_fit"""
-    clf = MultinomialNB(fit_prior=True)
-
     classes = range(11)
 
     for idx in range(len(train_data_50)):
         print("start loop")
+
         data = train_data_50[idx]
 
         X_data = [item[0] for item in data]
+        print(X_data)
         y_data = np.array([item[1] for item in data])
         print(y_data.shape)
         
         # had to reshape 3d array to 2d 
         X_sparse = np.array(map(sparse_matrix, X_data))
+        print(X_sparse.shape)
         new = X_sparse.reshape(50, 5001)
 
         print(new.shape)
@@ -64,7 +62,7 @@ def run_NB():
         clf.partial_fit(new, y_data, classes=classes)
 
 
-def score():
+def score(test_data):
     # need to break out test data same as above 
     X_test = [item[0] for item in test_data]
     y_test = [item[1] for item in test_data]
@@ -76,15 +74,25 @@ def score():
 
     print("NB score: {}".format(score))
 
-score()
 
-song_data = get_data()
+if __name__ == '__main__':
+    song_data = get_data()
 
-# spliting train/test 
-train_data = song_data[:500]
-test_data = song_data[500:550]
- 
-train_data_50 = chunker(train_data, 50)
+    # spliting train/test 
+    train_data = song_data[:500]
+    test_data = song_data[500:550]
+
+    # chunk data into an array of 50 long examples  
+    train_data_50 = chunker(train_data, 50)
+
+    # setup global NB model 
+    clf = MultinomialNB(fit_prior=True)
+
+    # train NB using partial fit 
+    run_NB(train_data_50)
+
+    # score the model using test data
+    score(test_data)
 
 
 

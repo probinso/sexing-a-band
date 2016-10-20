8059608413216b756b0e9068341e828985b0dce1
probinso <pmoss.robinson@gmail.com>
2016-10-20 14:57:00 -0700

    updates for terr

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+
+from __future__ import print_function
 import csv
 
 # Import all the sources modules
@@ -11,14 +13,17 @@ from lyrics_to_bow import lyrics_to_bow
 
 from random import shuffle
 
+from collections import defaultdict
+
 class Song:
+    sources = [wikia, lnm, az, mm, lm]
+    scores  = defaultdict(int)
     def __init__(self, year, ident, artist, title):
         self.year    = year
         self.ident   = ident
         self.artist  = artist
         self.title   = title
         self.lyrics  = None
-        self.sources = [wikia, lnm, az, mm, lm]
         self.error   = None
         self.bow     = None
 
@@ -29,22 +34,33 @@ class Song:
                 site(self)
                 if self.lyrics:
                     self.bow = lyrics_to_bow(self.lyrics)
+                    self.scores[site.__name__] += 1
                     break
-                
+
             except Exception as e:
                 print(e)
                 continue
 
-
+import csv
 def test():
-    fd = open("./tracks_per_year.txt")
-    for line in fd:
-        song = Song(*line.strip().split('<SEP>'))
-        song.download_lyrics()
-        print(song.bow)
-            
+    with open("/media/terra/UndecidedTeam/tracks_per_year.txt") as fd:
+        with open("/media/terra/UndecidedTeam/bow_runner.csv", 'w') as gd:
+            w = csv.writer(gd)
+
+            for line in fd:
+                song = Song(*line.strip().split('<SEP>'))
+                song.download_lyrics()
+                if song.bow:
+                    bow  = [':'.join([str(word), str(song.bow[word])]) for word in song.bow]
+                    meta = [song.year, song.title, song.artist]
+                    w.writerow(meta + bow)
+                print(song.scores)
+
+    with open("scores.txt", 'wb') as fd:
+        for i in Song.scores:
+            print(i, sum(scores[i]), sum(map(lambda x: 1 - x, scores[i])), file=fd)
 
 
-	
 if __name__ == '__main__':
     test()
+

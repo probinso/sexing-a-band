f8d0a2823ba978cc7cc7d6dccf457cb69cbe1177
probinso <pmoss.robinson@gmail.com>
2016-11-10 16:05:46 -0800

    remove lsi stuff

--- topic_maker_tfidf.py
+++ topic_maker_tfidf.py
@@ -3,17 +3,6 @@ from __future__ import print_function
 import csv
 import gensim
 
-with open('./data/mxm_dataset_train.txt') as fd:
-    line = '#'
-    it = iter(fd)
-    while line[0] == '#':
-        line = next(it)
-
-    raw = list(enumerate(line[1:].strip().split(','),1))
-
-lookup = {k : v for k, v in raw}
-
-# ---------------------------------------------------------------
 
 corpus = []
 with open("./data/full_output.csv") as fd:
@@ -24,14 +13,3 @@ with open("./data/full_output.csv") as fd:
 
 tfidf_model = gensim.models.TfidfModel(corpus, normalize=True)
 tfidf_model.save("./full_tfidf_model.tfidf")
-
-tfidf_corpus = []
-for item in corpus: 
-    tfidf_corpus.append(tfidf_model[item])
-    
-lsi = gensim.models.lsimodel.LsiModel(tfidf_corpus, num_topics=50, id2word=lookup)
-for index, topic in lsi.print_topics(num_topics=10, num_words = 3):
-    print(index)
-    print("  " + topic)
-
-lsi.save('./data/full_song_topics_tfidif.lsi')
